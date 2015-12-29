aeb8ad7926c25818ddb53897855fd3d4358e7d44
GoodKoding <info@goodkoding.com>
2015-12-29 20:31:14 +0600

    Fixed the JS and CSS to work properly on WP 4.4

--- js/admin.js
+++ js/admin.js
@@ -4,11 +4,11 @@
  * @author_url: http://goodkoding.com
  **/
 (function(){
-    jQuery('.gk-sslcommerz-options table.form-table').hide();
-    jQuery('.gk-sslcommerz-options h3').on('click', function(){
+    jQuery('.gk-sslcommerz-options .gk-sslcommerz-admin-form table.form-table').hide();
+    jQuery('.gk-sslcommerz-options .gk-sslcommerz-admin-form :header').on('click', function(){
         jQuery(this).next('table.form-table').toggle();
         jQuery(this).toggleClass('active');
     });
-    jQuery('.gk-sslcommerz-options h3').first().trigger('click');
-    jQuery('.gk-sslcommerz-options h3').append('<span class="dashicons dashicons-plus"></span>');
+    jQuery('.gk-sslcommerz-options .gk-sslcommerz-admin-form :header').first().trigger('click');
+    jQuery('.gk-sslcommerz-options .gk-sslcommerz-admin-form :header').append('<span class="dashicons dashicons-plus"></span>');
 })(jQuery);
